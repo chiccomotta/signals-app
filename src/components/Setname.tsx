06cbe28644386092c3be84fee46541f23ee800c5
Cristiano Motta <cristiano.motta@hotmail.it>
2024-03-13 09:26:02 +0100

    Added Faker library

--- src/components/Setname.tsx
+++ src/components/Setname.tsx
@@ -1,9 +1,10 @@
 import name from "../signals/name"
+import { fakerIT as faker } from "@faker-js/faker"
 
 export const Setname = () => {
   function update() {
-    const r = generateString(5)
-    const l = generateString(5)
+    const r = faker.person.firstName()
+    const l = faker.person.lastName()
 
     name.value = { name: r, surname: l }
   }
@@ -11,21 +12,9 @@ export const Setname = () => {
   return (
     <div>
       {/* <Input size="sm" value={text} onChange={(e) => setText(e.target.value)} /> */}
-      <button onClick={update}>Premi</button>
+      <button onClick={update} style={{ color: "blue", borderColor: "black" }}>
+        Premi
+      </button>
     </div>
   )
-}
-
-// program to generate random strings
-// declare all characters
-const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
-
-const generateString = (length: number) => {
-  let result = " "
-  const charactersLength = characters.length
-  for (let i = 0; i < length; i++) {
-    result += characters.charAt(Math.floor(Math.random() * charactersLength))
-  }
-
-  return result
 }
